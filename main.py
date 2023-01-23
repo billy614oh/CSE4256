97481766a49d4aca87ab743098abfb6e9b69a161
Billy Lee <122577217+billy614oh@users.noreply.github.com>
2023-01-23 21:29:30 +0000

    still working on merge

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
+def sum_largest(li):
+  max = 0
+  sec = 0
+  for element in li:
+    if element > max:
+      element = max
+      max = element
+  return max + sec
+def sumLargest(li):
+  li.sort()
+  return li[len(li - 1)] + li[len(li - 2]
+def merge(li1, li2):
+  li3 = []
+  i = 0
+  j = 0
+  while i < len(li1) and j < len(li2)
+    if li1(i)
+    li3.append(element)
+  return li3
+def largestSoFar(li):
+  count = 0;
+  prev = float("-inf")
+  for element in li:
+    if element > prev:
+      prev = element
+      count += 1
+  return count
+
+
+
 def evens(li):
   count = 0
   for i in range(len(li)):
@@ -6,6 +36,6 @@ def evens(li):
         count += 1
   return count
 
-li = [10, 8, 9, 9, 13, 16, 12]
 
+li = [10, 8, 9, 9, 13, 16, 12]
 print(evens(li))
